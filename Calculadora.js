837ad525adbbc5b78c47a455a6f32879cf0bb791
Samir Libos <78101874+SamLibos@users.noreply.github.com>
2021-09-27 21:38:50 -0300

    Calculadora

---
+++ Calculadora.js
@@ -0,0 +1,53 @@
+//CALCULADORA NIVEL 1 
+
+//somar
+
+function somar(num01, num02) {
+return num01+ num02;
+
+
+}
+console.log (somar(1,2));
+
+// Subtração
+
+function diminuir(num01, num02) {
+    return num01 - num02;
+    
+    
+    }
+    console.log (diminuir(2,1)); 
+      
+// multiplicar
+
+function multiplicar(num01, num02) {
+    return num01* num02;
+    
+    
+    }
+    console.log (multiplicar(2,5));
+
+ // Dividir  
+ 
+function dividir(num01, num02) {
+    return num01 / num02;
+    
+    
+    }
+    console.log (dividir(10,2));
+    
+//Crie uma função chamada quadradoDoNumero, que recebe um número como parâmetro e deve retornar esse número multiplicado por ele mesmo, ou seja, ao quadrado.
+//Importante: quadradoDoNumero() deve usar a função multiplicação() para calcular o quadrado do parâmetro inserido em power().
+//Crie a função mediaDeTresNumeros, ela deve calcular a média de 3 números, que serão inseridos por parâmetro.
+//Importante: Em mediaDeTresNumeros() você precisará usar algumas funções criadas anteriormente em nossa calculadora.
+
+
+//Crie a função calculaPorcentagem, que receberá dois parâmetros: o número total e a porcentagem que deseja calcular, e que deverá retornar x% de totalPorcentagem.
+//Exemplo: calculaPorcentagem(300, 15)  (deve retornar 45, pois é 15% de 300). 
+
+//Importante: calculaPorcentagem() você precisará usar algumas funções criadas anteriormente em nossa calculadora.
+
+
+//Crie uma função geradorDePorcentagem que leva dois parâmetros, e que deverá retornar a porcentagem do primeiro em relação ao segundo parâmetro.
+//Exemplo: geradorDePorcentagem (2, 200) (deve retornar 1 já que 2 é 1% de 200).
+
