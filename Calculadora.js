ca38177e5eed4fe27331921956a2f235f70e0abd
Samir Libos <78101874+SamLibos@users.noreply.github.com>
2021-10-04 15:34:55 -0300

    Add files via upload

--- Calculadora.js
+++ Calculadora.js
@@ -38,16 +38,45 @@ function dividir(num01, num02) {
     
 //Crie uma função chamada quadradoDoNumero, que recebe um número como parâmetro e deve retornar esse número multiplicado por ele mesmo, ou seja, ao quadrado.
 //Importante: quadradoDoNumero() deve usar a função multiplicação() para calcular o quadrado do parâmetro inserido em power().
+
+function quadradoDoNumero(num01) {
+    return multiplicar(num01,num01)
+
+}
+console.log(quadradoDoNumero(2));
+
+
+
 //Crie a função mediaDeTresNumeros, ela deve calcular a média de 3 números, que serão inseridos por parâmetro.
 //Importante: Em mediaDeTresNumeros() você precisará usar algumas funções criadas anteriormente em nossa calculadora.
+
+function mediaDeTresNumeros(num01,num02,num03){
+    let numeroTotalParcial = somar(num01,num02);
+    var total = (numeroTotalParcial + num03);
+    var resultado = dividir (total,3)
+    return resultado
+
+} 
+console.log (mediaDeTresNumeros(8,2,5));
 
 
 //Crie a função calculaPorcentagem, que receberá dois parâmetros: o número total e a porcentagem que deseja calcular, e que deverá retornar x% de totalPorcentagem.
 //Exemplo: calculaPorcentagem(300, 15)  (deve retornar 45, pois é 15% de 300). 
-
 //Importante: calculaPorcentagem() você precisará usar algumas funções criadas anteriormente em nossa calculadora.
 
+    function calculaPorcentagem (num01,num02){
+        let numeroTotalParcial = multiplicar(num01,num02);
+        var resultado = dividir (numeroTotalParcial,100)
+        return resultado;
+    }
+console.log (calculaPorcentagem(300,15))
 
 //Crie uma função geradorDePorcentagem que leva dois parâmetros, e que deverá retornar a porcentagem do primeiro em relação ao segundo parâmetro.
-//Exemplo: geradorDePorcentagem (2, 200) (deve retornar 1 já que 2 é 1% de 200).
+//Exemplo: geradorDePorcentagem (2, 200) (deve retornar 1 já que 2 é 1% de 200);
 
+function geradorDePorcentagem(num1, num2) {
+    let numeroTotal = multiplicar (num1, 100) 
+    var resultado = dividir (numeroTotal, num2)
+    return resultado+"%"
+}
+console.log (geradorDePorcentagem(2,200))
